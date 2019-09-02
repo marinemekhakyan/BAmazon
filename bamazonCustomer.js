aa922aa2e48249af4468782cb17b7679011be149
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-02 15:09:45 -0700

    adding function for listing available products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,27 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    listProducts();
+});
+
+function listProducts() {
+    connection.query("SELECT * FROM products ORDER BY product_name", function (err, results) {
+        if (err) throw err;
+        console.log("----------------------------");
+        console.log("Available items: ");
+        for (var i = 0; i < results.length; i++) {
+            console.log("\nID - " + results[i].item_id + "\nProduct Name - " + results[i].product_name + "\nPrice - " + "$" + results[i].price);
+            console.log("------------------------------");
+        }
+        pickItem();
+    });
+}
+
+// function pickItem() {
+//     inquirer.prompt({
+//         name: 
+//     })
+// }
