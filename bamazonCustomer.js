cbdf90f551608565cbbf71cc62c5c26d0102985a
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-02 15:37:10 -0700

    adding inquirer function and node modules

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,18 +18,58 @@ connection.connect(function(err) {
 function listProducts() {
     connection.query("SELECT * FROM products ORDER BY product_name", function (err, results) {
         if (err) throw err;
-        console.log("----------------------------");
+        console.log("<><><>><><><><><><><><><>");
         console.log("Available items: ");
         for (var i = 0; i < results.length; i++) {
             console.log("\nID - " + results[i].item_id + "\nProduct Name - " + results[i].product_name + "\nPrice - " + "$" + results[i].price);
-            console.log("------------------------------");
+            console.log("<><><>><><><><><><><><><>");
         }
         pickItem();
     });
 }
 
-// function pickItem() {
-//     inquirer.prompt({
-//         name: 
-//     })
-// }
+function pickItem() {
+    inquirer.prompt([{
+        name: "cSelection",
+        type: "input",
+        message: "Please select an item by its ID.",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            console.log("<><><>><><><><><><><><><>")
+            console.log("You might want to check that ID number again:)")
+            return false;
+        }
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            console.log("<><><>><><><><><><><><><>");
+            console.log("Please provide a number.")
+            return false;
+        }
+    } 
+]).then
+(function (answers) {
+    connection.query(
+        "SELECT * FROM products WHERE item_id = " + answers.cSelection, function (err, results) {
+            if (err) throw err;
+            if (quantity > results[0].stock_quantity) {
+                console.log("Sorry, we don't have that many in stock. Try a smaller quantity!")
+                endConnection();
+                return;
+            }
+            else {
+                updateStock();
+            }
+        }
+    )
+}
+)
+}
