2022cd2c8c79f8f2424c41818a0c0a61e989eff7
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-09-04 20:37:51 -0700

    final formating

--- bamazon.sql
+++ bamazon.sql
@@ -14,13 +14,13 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES
-("Energy: A Beginner's Guide by Waclav Slim", "Books", 10.84, 44),
-("Wireless Bluetooth Earbuds", "Electronics", 47.99, 8),
-("LED Floor Lamp", "Furniture", 129.99, 3),
-("Cartilage Ear Cuff", "Jewelry", 150.0, 5),
-("Lavazza Caffe Espresso", "Grocery", 19.44, 1),
-("The Age of Collage Vol.2", "Books", 115.0, 2),
+("Energy: A Beginner's Guide by Waclav Slim", "Books", 10.84, 10),
+("Wireless Bluetooth Earbuds", "Electronics", 47.99, 10),
+("LED Floor Lamp", "Furniture", 129.99, 10),
+("Cartilage Ear Cuff", "Jewelry", 150.0, 10),
+("Lavazza Caffe Espresso", "Grocery", 19.44, 10),
+("The Age of Collage Vol.2", "Books", 115.0, 10),
 ("Thou Shall Not Try Me Women's T-shirt", "Clothing", 28.60, 10),
-("Adidas Squad Duffle Bag", "Sports Accessories", 36.99, 3),
-("Big Joe Milano Bean Bag", "Furniture", 81.64, 2),
-("Teamonk Oolong Tea", "Grocery", 12.49, 14);
+("Adidas Squad Duffle Bag", "Sports Accessories", 36.99, 10),
+("Big Joe Milano Bean Bag", "Furniture", 81.64, 10),
+("Teamonk Oolong Tea", "Grocery", 12.49, 10);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
 var connection = mysql.createConnection({
     host: "localhost",
-    port:3306,
+    port: 3306,
     user: "root",
     password: "",
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     listProducts();
@@ -19,12 +18,12 @@ connection.connect(function(err) {
 function listProducts() {
     connection.query("SELECT * FROM products ORDER BY product_name ASC", function (err, results) {
         if (err) throw err;
-        console.log("<><><><><><><><><><><><><>");
+        console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
         console.log("Available items: ");
         for (var i = 0; i < results.length; i++) {
             console.log("\nID - " + results[i].item_id + "\nProduct Name - " + results[i].product_name + "\nPrice - " + "$" + results[i].price);
-            console.log("<><><><><><><><><><><><><>");
-            var price = results[i].price;            
+            console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
+            var price = results[i].price;
         }
         pickItem();
     });
@@ -34,13 +33,12 @@ function pickItem() {
     inquirer.prompt([{
         name: "cSelection",
         type: "input",
-        message: "Please select an item by its ID.",
-        validate: function(value) {
+        message: "Welcome! We B Amazon:) Please select an item by its ID.",
+        validate: function (value) {
             if (isNaN(value) === false) {
                 return true;
             }
-            console.log("<><><><><><><><><><><><><>")
-            console.log("You might want to check that ID number again:)");
+            console.log("Please make sure you're providing the right item ID.");
             return false;
         }
     },
@@ -52,31 +50,30 @@ function pickItem() {
             if (isNaN(value) === false) {
                 return true;
             }
-            console.log("<><><><><><><><><><><><><>")
+            console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>")
             console.log("Please provide a number.");
             return false;
         }
-    } 
-]).then
-(function (answers) {
-    // var price = results[i].price;
-    var customerSelect = answers.cSelection;
-    var quantity = answers.quantity;
-    connection.query(
-        "SELECT * FROM products WHERE item_id = " + answers.cSelection, function (err, results) {
-            if (err) throw err;
-            var result = results[0]
-            var price = result.price;
-            if (quantity > results[0].stock_quantity) {
-                console.log("Sorry, we don't have that many in stock. Try a smaller quantity!")
-                end();
-                return;
-            }
-            else {
-                updateStock(price, quantity, customerSelect);
-            }
+    }
+    ]).then
+        (function (answers) {
+            var customerSelect = answers.cSelection;
+            var quantity = answers.quantity;
+            connection.query(
+                "SELECT * FROM products WHERE item_id = " + answers.cSelection, function (err, results) {
+                    if (err) throw err;
+                    var result = results[0]
+                    var price = result.price;
+                    if (quantity > results[0].stock_quantity) {
+                        console.log("Sorry, we don't have that many in stock. Try a smaller quantity!")
+                        end();
+                        return;
+                    }
+                    else {
+                        updateStock(price, quantity, customerSelect);
+                    }
+                });
         });
-});
 }
 
 function updateStock(price, quantity, customerSelect) {
